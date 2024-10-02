fa5c413de1b6dd04ebc70ad92c58c62db7adf1da
Aaron Gould <158851930+aarixx3@users.noreply.github.com>
2024-10-02 16:53:53 -0400

    Add files via upload

---
+++ landmark.py
@@ -0,0 +1,79 @@
+# Aaron Gould
+# COP 2500
+# Landmark
+# Jan 17, 2023
+
+# Flag of Turkey
+# I had to use color to make the shapes 
+
+import turtle
+
+turtle.fillcolor('red')
+turtle.begin_fill()
+turtle.pencolor('red')
+turtle.forward(300)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(300)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.end_fill()
+
+turtle.penup()
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.pendown()
+turtle.left(90)
+
+turtle.fillcolor('white')
+turtle.begin_fill()
+turtle.pencolor('red')
+turtle.circle(-50)
+turtle.end_fill()
+
+turtle.right(90)
+turtle.penup()
+turtle.forward(100)
+turtle.pendown()
+
+turtle.left(90)
+turtle.fillcolor('red')
+turtle.begin_fill()
+turtle.pencolor('red')
+turtle.circle(40)
+turtle.end_fill()
+
+turtle.pencolor('white')
+turtle.fillcolor('white')
+turtle.begin_fill()
+turtle.right(70)
+turtle.forward(20)
+turtle.left(70)
+turtle.forward(20)
+turtle.right(140)
+turtle.forward(20)
+turtle.left(70)
+turtle.forward(20)
+turtle.right(140)
+turtle.forward(20)
+turtle.left(70)
+turtle.forward(20)
+turtle.right(140)
+turtle.forward(20)
+turtle.left(70)
+turtle.forward(20)
+turtle.right(140)
+turtle.forward(20)
+turtle.left(57)
+turtle.forward(20)
+turtle.end_fill()
+turtle.pencolor('black')
+
+turtle.penup()
+turtle.forward(200)
+
+
